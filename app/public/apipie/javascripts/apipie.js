b2dbd92d5033aa0b29f6782a8845b2e20e5f0358
Lior Sion <lior.sion@gmail.com>
2013-10-01 10:04:58 +0300

    javascript part

--- app/public/apipie/javascripts/apipie.js
+++ app/public/apipie/javascripts/apipie.js
@@ -4,3 +4,28 @@ $(document).ready(function() {
     prettyPrint();
   }
 });
+
+function execute() {
+	var valid = true;
+	var collected_data = {};
+
+  $.each( $("input[data-role]='parameter'"), function( key, value ) {
+  	if (value.required && !$(value).val()) {
+  		valid = false;
+  		$(value).parents('td').addClass('warning');
+  	}
+  	collected_data[value.id] = $(value).val();
+  	
+  });
+
+  if (!valid) {
+  	alert('Not valid');
+  	return;
+  }
+  url = $("#method-data").data("url");
+  method = $("#method-data").data("method");
+  $.ajax({ url: url,
+  	       type: method,
+  	       data: collected_data
+  	     })
+}
